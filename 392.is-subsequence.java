579ee7038137d414f8ebc9540461437743ad3994
mehul425 <m.makwana450@gmail.com>
2023-05-18 17:57:20 +0530

    392, 58

---
+++ 392.is-subsequence.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=392 lang=java
+ *
+ * [392] Is Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int j = 0;
+        for (int i = 0; i < t.length() && j < s.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+        }
+        if (j == s.length()) {
+            return true;
+        }
+        return false;
+    }
+}
+// @lc code=end
+
+""  "aec"
+0
